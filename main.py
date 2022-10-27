a5edf9aeb2925c91347149e50cec1a71b0204c9e
Cheung <cheung@tdfcu.com>
2022-10-27 15:22:15 -0400

    Added password generator function and added pyperclip to automatically copy password to clipboard

--- main.py
+++ main.py
@@ -1,8 +1,37 @@
 import tkinter
-from tkinter import messagebox
+import random
+import pyperclip
+from tkinter import END, messagebox
 
+# Password Generator
+
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+               'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    random_letters = [random.choice(letters) for char in range(nr_letters)]
+
+    random_symbols = [random.choice(symbols) for char in range(nr_symbols)]
+
+    random_numbers = [random.choice(numbers) for char in range(nr_numbers)]
+
+    password_list = random_letters + random_symbols + random_numbers
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.insert(tkinter.END, password)
+    pyperclip.copy(password)
 
 # Save Password Function
+
+
 def save():
     website = website_entry.get()
     email = email_entry.get()
@@ -48,7 +77,8 @@ password_label = tkinter.Label(text='Password:')
 password_label.grid(row=3, column=0)
 password_entry = tkinter.Entry(width=21)
 password_entry.grid(row=3, column=1)
-password_button = tkinter.Button(text='Generate Password')
+password_button = tkinter.Button(
+    text='Generate Password', command=generate_password)
 password_button.grid(row=3, column=2)
 
 add_button = tkinter.Button(text='Add', width=36, command=save)
