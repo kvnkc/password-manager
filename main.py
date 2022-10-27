dc230cf057bc31115050c8358993cec0f99c0a63
Cheung <cheung@tdfcu.com>
2022-10-27 12:45:06 -0400

    added save password function where information can be saved and cleared when add button is clicked

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import tkinter
 
+
+# Save Password Function
+def save():
+    with open('data.txt', 'a') as passwords:
+        passwords.write(
+            f'{email_entry.get()} | {website_entry.get()} | {password_entry.get()}\n')
+        website_entry.delete(0, tkinter.END)
+        password_entry.delete(0, tkinter.END)
+
+
 # Password Manager UI
 window = tkinter.Tk()
 window.title('Password Manager')
@@ -14,10 +24,12 @@ website_label = tkinter.Label(text='Website:')
 website_label.grid(row=1, column=0)
 website_entry = tkinter.Entry(width=35)
 website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()  # starts cursor at website entry
 
 email_label = tkinter.Label(text='Email/Username:')
 email_label.grid(row=2, column=0)
 email_entry = tkinter.Entry(width=35)
+email_entry.insert(tkinter.END, 'kekecheung@gmail.com')
 email_entry.grid(row=2, column=1, columnspan=2)
 
 password_label = tkinter.Label(text='Password:')
@@ -27,7 +39,7 @@ password_entry.grid(row=3, column=1)
 password_button = tkinter.Button(text='Generate Password')
 password_button.grid(row=3, column=2)
 
-add_button = tkinter.Button(text='Add', width=36)
+add_button = tkinter.Button(text='Add', width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
