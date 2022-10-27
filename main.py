ae0739e68910ed6c243472f52c94761227d71b64
Cheung <cheung@tdfcu.com>
2022-10-27 12:08:58 -0400

    added labels, buttons, and entries to ui

--- main.py
+++ main.py
@@ -4,11 +4,30 @@ import tkinter
 window = tkinter.Tk()
 window.title('Password Manager')
 window.minsize(width=200, height=200)
-window.config(padx=20, pady=20)
+window.config(padx=40, pady=40)
 canvas = tkinter.Canvas(width=200, height=200)
 lock = tkinter.PhotoImage(file='logo.png')
 canvas.create_image(100, 100, anchor='center', image=lock)
-canvas.pack()
+canvas.grid(row=0, column=1)
 
+website_label = tkinter.Label(text='Website:')
+website_label.grid(row=1, column=0)
+website_entry = tkinter.Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=2)
+
+email_label = tkinter.Label(text='Email/Username:')
+email_label.grid(row=2, column=0)
+email_entry = tkinter.Entry(width=35)
+email_entry.grid(row=2, column=1, columnspan=2)
+
+password_label = tkinter.Label(text='Password:')
+password_label.grid(row=3, column=0)
+password_entry = tkinter.Entry(width=21)
+password_entry.grid(row=3, column=1)
+password_button = tkinter.Button(text='Generate Password')
+password_button.grid(row=3, column=2)
+
+add_button = tkinter.Button(text='Add', width=36)
+add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
